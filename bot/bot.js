f41f0bb4b93a650d88fd489d4290c1d95880e463
dimas <dimasmaulana0305@gmail.com>
2024-07-05 08:35:55 +0800

    chore: Refactor getBrowser function and improve browser initialization

--- bot/bot.js
+++ bot/bot.js
@@ -28,7 +28,7 @@ function sleep(s) {
     return new Promise((resolve) => setTimeout(resolve, s))
 }
 
-function getBrowser(){
+function getBrowser() {
     const browserPaths = [
         "/usr/bin/chromium-browser",
         "/usr/bin/google-chrome",
@@ -42,31 +42,37 @@ function getBrowser(){
     throw new Error("No browser found")
 }
 
-const initBrowser = puppeteer.launch({
-    executablePath: getBrowser(),
-    headless: false,
-    args: [
-        '--disable-dev-shm-usage',
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-gpu',
-        '--no-gpu',
-        '--disable-default-apps',
-        '--disable-translate',
-        '--disable-device-discovery-notifications',
-        '--disable-software-rasterizer',
-        '--disable-xss-auditor',
-        ...(() => {
-            if (CONFIG.APPEXTENSIONS === "") return [];
-            return [
-                `--disable-extensions-except=${CONFIG.APPEXTENSIONS}`,
-                `--load-extension=${CONFIG.APPEXTENSIONS}`
-            ]
-        })()
-    ],
-    ipDataDir: '/home/bot/data/',
-    ignoreHTTPSErrors: true
-});
+/**
+ * @type {puppeteer.LaunchOptions}
+ **/
+const browserArgs = {
+        executablePath: getBrowser(),
+        headless: false,
+        args: [
+            '--disable-dev-shm-usage',
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-gpu',
+            '--no-gpu',
+            '--disable-default-apps',
+            '--disable-translate',
+            '--disable-device-discovery-notifications',
+            '--disable-software-rasterizer',
+            '--disable-xss-auditor',
+            ...(() => {
+                if (CONFIG.APPEXTENSIONS === "") return [];
+                return [
+                    `--disable-extensions-except=${CONFIG.APPEXTENSIONS}`,
+                    `--load-extension=${CONFIG.APPEXTENSIONS}`
+                ]
+            })(),
+        ],
+        ignoreHTTPSErrors: true
+    }
+/**
+ * @type {puppeteer.Browser}
+ */
+var initBrowser = null;
 
 console.log("Bot started...");
 
@@ -78,13 +84,22 @@ module.exports = {
         max: CONFIG.APPLIMIT
     },
     bot: async (urlToVisit) => {
-        const browser = await initBrowser;
-        const context = await browser.createBrowserContext()
+        var context = null;
+        // use the same browser instance for all bots if no extensions are loaded
+        if (CONFIG.APPEXTENSIONS === "") {
+            if (initBrowser === null) {
+                initBrowser = await puppeteer.launch(browserArgs);
+            }
+            context = await initBrowser.createBrowserContext();
+        }
+        // create a new browser instance for each bot if extensions are loaded
+        // this is because puppeteer does not add the extensions to new browser contexts
+        if (CONFIG.APPEXTENSIONS !== "") {
+            context = (await puppeteer.launch(browserArgs)).defaultBrowserContext();
+        }
         try {
-            // Goto main page
             const page = await context.newPage();
 
-            // Set Flag
             await page.setCookie({
                 name: "flag",
                 httpOnly: false,
@@ -92,21 +107,23 @@ module.exports = {
                 url: CONFIG.APPURL
             })
 
-            // Visit URL from user
             console.log(`bot visiting ${urlToVisit}`)
             await page.goto(urlToVisit, {
                 waitUntil: 'networkidle2'
             });
             await sleep(15000);
 
-            // Close
             console.log("browser close...")
-            await context.close()
             return true;
         } catch (e) {
             console.error(e);
-            await context.close();
             return false;
+        } finally {
+            if (CONFIG.APPEXTENSIONS !== "") {
+                await context.browser().close();
+            }else{
+                await context.close();
+            }
         }
     }
 }
